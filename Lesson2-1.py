023efbe1ec8d5cf711184a0fe12ba415faa95f15
ExcalibrX <hector_rodriguez2417@hotmail.com>
2025-09-21 22:37:34 -0400

    Add files via upload

---
+++ Lesson2-1.py
@@ -0,0 +1,4 @@
+myCurrentAge=int(input ("What is your current age?"))
+currentyear=int(input ("What is the current year?"))
+myNewAge=str(myCurrentAge + (2050 - currentyear)) 
+print ("Your age in 2050 will be..."  + myNewAge )
